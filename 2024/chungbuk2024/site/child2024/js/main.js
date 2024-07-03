0d476af566a083237be5c2dd67e5d44d96a15735
YangGaeng <85447479+shallow960@users.noreply.github.com>
2024-07-03 17:55:59 +0900

    슬릭 개수 수정

--- 2024/chungbuk2024/site/child2024/js/main.js
+++ 2024/chungbuk2024/site/child2024/js/main.js
@@ -165,7 +165,7 @@ try {
 
 				$OldImgList.slick({
 					accessibility: true, //접근성 여부
-					autoplay : true,
+					autoplay : false,
 					autoplaySpeed : 0,
 					speed : 2500,
 					dots : false,
@@ -173,7 +173,7 @@ try {
 					swipe : true,
 					swipeToSlide : true,
 					slidesToShow : 3,
-					slidesToScroll : 1,
+					slidesToScroll : 2,
 					variableWidth : true,
 					infinite: true,
 					arrows : true,
